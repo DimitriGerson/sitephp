521c35cf5a4bd2ea96cbf37af30b9965a8a11c52
Dimitri Gerson <dimitri.gerson@gmail.com>
2021-09-13 21:14:58 +0200

    ajout d'un fichier squelette qui sera une base.

---
+++ squelette.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Dimitri Gerson - Le Site Web</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Dimitri Gerson</h1>
+            <h2>Présentation</h2>
+        </header>
+        
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">CV</a></li>
+            </ul>
+        </nav>
+        
+        <section>
+            <aside>
+                <h1>À propos de l'auteur</h1>
+                <p>Bonjour à tous ! Après une carrière dans la photographie, je me suis formée d'une manière générale à l'informatique en .</p>
+            </aside>
+            <article>                
+                <h1>Je suis un grand voyageur</h1>
+                <p>Bla bla bla bla (texte de l'article)</p>
+            </article>
+        </section>
+        
+        <footer>
+            <p>Copyright Zozor - Tous droits réservés<br />
+            <a href="#">Me contacter !</a></p>
+        </footer>
+        
+    </body>
+</html><!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Zozor - Le Site Web</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Zozor</h1>
+            <h2>Carnets de voyage</h2>
+        </header>
+        
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">CV</a></li>
+            </ul>
+        </nav>
+        
+        <section>
+            <aside>
+                <h1>À propos de l'auteur</h1>
+                <p>C'est moi, Zozor ! Je suis né un 23 novembre 2005.</p>
+            </aside>
+            <article>                
+                <h1>Je suis un grand voyageur</h1>
+                <p>Bla bla bla bla (texte de l'article)</p>
+            </article>
+        </section>
+        
+        <footer>
+            <p>Copyright Zozor - Tous droits réservés<br />
+            <a href="#">Me contacter !</a></p>
+        </footer>
+        
+    </body>
+</html>

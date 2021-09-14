7c66e661640fd02b5740f7c16b85d9ee4b70a850
Dimitri Gerson <dimitri.gerson@gmail.com>
2021-09-14 21:51:36 +0200

    Il manquait le doctype ainsi que les variables.

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+$nompage="Guide incrémental du VAE";
+$titre="Le VAE";
+$soustitre="Un vélo pour de longues distances";
+$titrepresentation="Un VAE quelles utilisations ?";
+$textepresentation="Le VAE le plaisir de la route.";
+$titreaericle="Quel VAE acheter ?";
+$textarticle="l'important étant d'avoir un vélo adapté.";
 
 include("head.inc.php");
 include("nav.inc.php");

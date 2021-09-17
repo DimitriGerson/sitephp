8c529b94315f1bbddb394e5de6e24baf27f2b604
Dimitri Gerson <dimitri.gerson@gmail.com>
2021-09-17 14:33:45 +0200

    ajout du fichier de style.css dans son dossier, modification du doctype.inc.php

--- doctype.inc.php
+++ doctype.inc.php
@@ -3,8 +3,9 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<!-- ajouter les fichier pour css javascript et bootstrap -->
-<?php
 
+<?php
+echo "<link rel='stylesheet' type='text/css' href='styles/style.css' media='all'>";
 echo "<title>$nompage</title>";
 ?>
 

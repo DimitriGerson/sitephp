dc4cae7b88364508fcfef7834f3e570835018b8e
Dimitri Gerson <dimitri.gerson@gmail.com>
2021-09-21 17:48:52 +0200

    avancement sur le doctype.inc.php avec mise en place de langue et bootstrap5 création d'une nav bar sur le fichier nav.inc.php

--- doctype.inc.php
+++ doctype.inc.php
@@ -1,9 +1,233 @@
 <!DOCTYPE html>
-<html lang="fr">
-	<head>
+<html class="no-js -webkit-"lang="fr">
+<head>
+<style>
+ body {transition: opacity ease-in 0.2s; }
+ body[unresolved] {opacity: 0; display: block; overflow: hidden; position: relative; }
+</style>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<!-- ajouter les fichier pour css javascript et bootstrap -->
+<meta charset="UTF-8">
+<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+<style media data-href="https://fonts.googleapis.com/css?family=Arimo:400,400i,700,700i|Rokkitt|Source+Sans+Pro:100,300,400,500,600,900">
+/*# sourceURL=https://fonts.googleapis.com/css?family=Arimo:400,400i,700,700i|Rokkitt|Source+Sans+Pro:100,300,400,500,600,900 */
 
+/*@ sourceURL=https://fonts.googleapis.com/css?family=Arimo:400,400i,700,700i|Rokkitt|Source+Sans+Pro:100,300,400,500,600,900 */
+/*cyrillic-ext */	
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9C5kiK-u.woff2) format('woff2');
+	unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
+}
+/* cyrillic */
+
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CdkiK-u.woff2) format('woff2');
+	unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
+}
+/* greek-ext */
+
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9C9kiK-u.woff2) format('woff2');
+	unicode-range: U+1F00-1FFF;
+}
+/* greek */
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CBkiK-u.woff2) format('woff2');
+	unicode-range: U+0370-03FF;
+}
+/* hebrew */
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CFkiK-u.woff2) format('woff2');
+	unicode-range: U+0590-05FF, U+20AA, U+25CC, U+FB1D-FB4F;
+}
+/* vietnamese */
+
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CxkiK-u.woff2) format('woff2');
+	unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
+}
+/* latin-ext */
+
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9C1kiK-u.woff2) format('woff2');
+	unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CNkiA-u.woff2) format('woff2');
+	unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+/*cyrillic-ext */
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 700;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9C5kiK-u.woff2) format('woff2');
+	unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
+}
+/* cyrillic */
+
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 700;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CdkiK-u.woff2) format('woff2');
+	unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
+}
+/* greek-ext */
+
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 700;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9C9kiK-u.woff2) format('woff2');
+	unicode-range: U+1F00-1FFF;
+}
+/* greek */
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 700;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CBkiK-u.woff2) format('woff2');
+	unicode-range: U+0370-03FF;
+}
+/* hebrew */
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 700;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CFkiK-u.woff2) format('woff2');
+	unicode-range: U+0590-05FF, U+20AA, U+25CC, U+FB1D-FB4F;
+}
+/* vietnamese */
+
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 700;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CxkiK-u.woff2) format('woff2');
+	unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
+}
+/* latin-ext */
+
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 700;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9C1kiK-u.woff2) format('woff2');
+	unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+	font-family: 'Arimo';
+	font-style: italic;
+	font-weight: 700;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CNkiA-u.woff2) format('woff2');
+	unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+/*cyrillic-ext */
+@font-face {
+	font-family: 'Arimo';
+	font-style: normal;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9C5kiK-u.woff2) format('woff2');
+	unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
+}
+/* cyrillic */
+
+@font-face {
+	font-family: 'Arimo';
+	font-style: normal;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CdkiK-u.woff2) format('woff2');
+	unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116;
+}
+/* greek-ext */
+
+@font-face {
+	font-family: 'Arimo';
+	font-style: normal;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9C9kiK-u.woff2) format('woff2');
+	unicode-range: U+1F00-1FFF;
+}
+/* greek */
+@font-face {
+	font-family: 'Arimo';
+	font-style: normal;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CBkiK-u.woff2) format('woff2');
+	unicode-range: U+0370-03FF;
+}
+/* hebrew */
+@font-face {
+	font-family: 'Arimo';
+	font-style: normal;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CFkiK-u.woff2) format('woff2');
+	unicode-range: U+0590-05FF, U+20AA, U+25CC, U+FB1D-FB4F;
+}
+/* vietnamese */
+
+@font-face {
+	font-family: 'Arimo';
+	font-style: normal;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CxkiK-u.woff2) format('woff2');
+	unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
+}
+/* latin-ext */
+
+@font-face {
+	font-family: 'Arimo';
+	font-style: normal;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9C1kiK-u.woff2) format('woff2');
+	unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
+/* latin */
+@font-face {
+	font-family: 'Arimo';
+	font-style: normal;
+	font-weight: 400;
+	src: url(https://fonts.gstatic.com/s/arimo/v17/P5sCzZCDf9_T_10c9CNkiA-u.woff2) format('woff2');
+	unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+</style>
+	<link rel="canonical" href="<?php echo '$adressesite' ?>">
+	<meta name="title" content="<?php echo '$nomsite' ?>">
+	<meta name="keymords" content="<?php echo '$motclefs' ?>">
+	<meta name="description" content="<?php echo '$description' ?>">
+	<meta name="language" content="<?php echo '$langue' ?>">
+	<meta name="robots" content="<?php echo '$robots' ?>">
+	<!-- ajouter les fichier pour css javascript et bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 <?php
 echo "<link rel='stylesheet' type='text/css' href='styles/style.css' media='all'>";
 echo "<title>$nompage</title>";
